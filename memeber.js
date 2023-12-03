b8166790e7a51fa58ed24689ecb4fbc977d7a0a0
gowthamc7 <78102989+gowthamc7@users.noreply.github.com>
2023-12-03 16:28:55 +0000

    copilot second commit

---
+++ memeber.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberTextLength = skillsMemberText.length;
+    var skillsMemberTextValue = skillsMemberText.substring(0, skillsMemberTextLength - 1);
+    var skillsMemberTextValueLength = skillsMemberTextValue.length;
+    var skillsMemberTextValueLast = skillsMemberTextValue.substring(skillsMemberTextValueLength - 1, skillsMemberTextValueLength);
+    var skillsMemberTextValueLastNumber = parseInt(skillsMemberTextValueLast);
+    var skillsMemberTextValueLastNumberPlus = skillsMemberTextValueLastNumber + 1;
+    var skillsMemberTextValueLastNumberPlusString = skillsMemberTextValueLastNumberPlus.toString();
+    var skillsMemberTextValueLastNumberPlusStringLength = skillsMemberTextValueLastNumberPlusString.length;
+    var skillsMemberTextValueLastNumberPlusStringLast = skillsMemberTextValueLastNumberPlusString.substring(skillsMemberTextValueLastNumberPlusStringLength - 1, skillsMemberTextValueLastNumberPlusStringLength);
+    var skillsMemberTextValueLastNumberPlusStringLastNumber = parseInt(skillsMemberTextValueLastNumberPlusStringLast);
+    var skillsMemberTextValueLastNumberPlusStringLastNumberPlus = skillsMemberTextValueLastNumberPlusStringLastNumber + 1;
+    var skillsMemberTextValueLastNumberPlusStringLastNumberPlusString = skillsMemberTextValueLastNumberPlusStringLastNumberPlus.toString();
+    var skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLength = skillsMemberTextValueLastNumberPlusStringLastNumberPlusString.length;
+    var skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLast = skillsMemberTextValueLastNumberPlusStringLastNumberPlusString.substring(skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLength - 1, skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLength);
+    var skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLastNumber = parseInt(skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLast);
+    var skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLastNumberPlus = skillsMemberTextValueLastNumberPlusStringLastNumberPlusStringLastNumber + 1;
+    var skillsMemberTextValueLastNumberPlusStringLastNumberPlus
